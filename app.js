2b03dad47e74e3c1b57fa6b958e3dc1a2e658064
LaceyBaughman <laceyebaughman@gmail.com>
2022-02-08 17:11:28 -0700

    Afternoon Draft

---
+++ app.js
@@ -0,0 +1,64 @@
+
+let packages = [{ heavy: true, priority: false, fragile: false, image: '/assets/images/Package.png', lost: false, name: 'Zach', Brand: 'Amazon' },
+{ heavy: false, priority: true, fragile: true, image: '/assets/images/Package.png', name: 'Jonesy', lost: false, Brand: 'Costco' },
+{ heavy: true, priority: false, fragile: true, image: '/assets/images/Package.png', name: 'Jake', lost: false, Brand: 'Costco' },
+{ heavy: false, priority: false, fragile: false, image: '/assets/images/Package.png', name: 'Mark', lost: false, Brand: 'Wayfair' },
+{ heavy: true, priority: true, fragile: true, image: '/assets/images/Package.png', name: 'Brittany', lost: false, Brand: 'Wayfair' },
+{ heavy: false, priority: true, fragile: false, image: '/assets/images/Package.png', name: 'Mick', lost: false, Brand: 'Amazon' },
+{ heavy: true, priority: false, fragile: true, image: '/assets/images/Package.png', name: 'Jeremy', lost: false, Brand: 'Ikea' }]
+
+
+let search = []
+
+function draw() {
+  let template = ""
+  for (let i = 0; i < search.length; i++) {
+    const package = search[i];
+    template += `
+
+      <div class="col-md-3 d-flex justify-content-center">
+        <div onclick="guess('${package.name}')">
+          <img class="img-fluid" package-image src="${package.image}">
+            <div class="p-2 text-center">
+              <h3>${package.name}</h3>
+            </div>
+        </div>
+      </div>
+      `
+  }
+  document.getElementById('cards').innerHTML = template
+}
+
+
+function newGame() {
+  packages.forEach(package => package.lost = false)
+
+  const randIndex = Math.floor(Math.random() * packages.length)
+  packages[randIndex].lost = true
+  console.log(packages[randIndex])
+  search = packages
+  draw()
+}
+
+
+
+function filter(filteredProperty) {
+  let lostPackage = packages.find(package => package.lost)
+  search = search.filter(package => package[filteredProperty] === lostPackage[filteredProperty])
+  draw()
+}
+
+
+function guess(name) {
+  let lostPackage = packages.find(package => package.lost)
+  if (lostPackage.name == name) {
+    alert('Package found!')
+  }
+  else {
+    alert('Wrong! Keep Searching!')
+  }
+}
+
+
+newGame()
+
